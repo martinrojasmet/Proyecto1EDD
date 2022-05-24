dd8787809578cf04da98a823a98027707a9d653d
modosenin0 <97416036+modosenin0@users.noreply.github.com>
2022-05-24 08:13:19 -0400

    .3

--- ProjectOrganizedChaos/src/projectorganizedchaos/ProjectOrganizedChaos.java
+++ ProjectOrganizedChaos/src/projectorganizedchaos/ProjectOrganizedChaos.java
@@ -1,20 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package projectorganizedchaos;
 
-/**
- *
- * @author marti
- */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import javax.swing.JOptionPane;
+
+
 public class ProjectOrganizedChaos {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola");
+//        SimpleListWithArray list = readTextFile();
+//        list.printList();
+
         
     }
     
